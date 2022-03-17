782d196b2c6609d8f22d85ff4d15d8a90b4b6f27
root <abeeb.ilias@gmail.com>
2022-03-17 08:58:52 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     *
+ */
+int main(void)
+{
+	 char *str0, str;
+         int inc;
+         inc = 0;
+         str0 = "_putchar";
+         str = str0[inc];
+         while (str != '\0')
+         {
+		 putchar(str);											                         inc++;
+ 		 str = str0[inc];			
+	 }
+	 putchar('\n');
+	 return (0);
+}
