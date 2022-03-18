d374831831b88c4c208535f32f62bf55962299e3
root <abeeb.ilias@gmail.com>
2022-03-18 11:01:20 -0700

    Added 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,18 +8,21 @@ void more_numbers(void)
 {
 	int j;
 	int i;
-	
-	for (j = 0; j <= 10; j++)
+
+	j = 0;
+	i = 0;
+	while (j <= 10)
 	{
-		for (i = 0; i <= 14; i++) 
+		while (i <= 14) 
 		{
 			if (i > 9)
 			{
 				_putchar(i / 10 + '0');
 				_putchar(i % 10 + '0');
 			}
-
+			i++;
 		}
+		j++;
 		putchar ('\n');
 	}
 	return (0);
