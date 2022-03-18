f3306bfea802910352dc7d6e07a15caef5922c4b
root <abeeb.ilias@gmail.com>
2022-03-18 11:33:06 -0700

    Added 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,12 +1,8 @@
-/*
- * File: 6-print_line.c
- * Auth: Brennan D Baraban
- */
-
-#include "main.h"
+#include <stdio.h>
 
 /**
  * print_line - Draws a straight line using the character _.
+ *
  * @n: The number of _ characters to be printed.
  */
 void print_line(int n)
@@ -16,7 +12,8 @@ void print_line(int n)
 	if (n > 0)
 	{
 	for (len = 0; len < n; len++)
-		_putchar('_');
+		putchar('_');
 	}
-	_putchar('\n');
+	//_putchar('\n');
+	return (0);
 }
